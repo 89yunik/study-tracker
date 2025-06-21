a4ae490810ec929a922e63ed8494a54ae1361d64
Nick Yoon <89yunik@gmail.com>
2025-06-21 12:05:45 +0900

    feat: Solve BOJ #27172 수 나누기 게임

---
+++ topics/algorithm/BOJ_27172_수 나누기 게임.py
@@ -0,0 +1,10 @@
+N=int(input())
+cards=[*map(int, input().split())]
+scores = {}
+for card in cards: scores[card] = 0
+for card in cards: 
+    multiple = card*2
+    while multiple <= 10**6:
+        if multiple in scores: scores[card]+=1; scores[multiple] -= 1
+        multiple+=card
+for card in cards: print(scores[card])
